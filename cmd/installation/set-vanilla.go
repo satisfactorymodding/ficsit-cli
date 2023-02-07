31ef8ee79266dc91a7150c2f3d460248447c0131
mircearoata <mircearoatapalade@gmail.com>
2023-02-07 15:01:04 +0100

    fix: use viper for the set-vanilla off flag

--- cmd/installation/set-vanilla.go
+++ cmd/installation/set-vanilla.go
@@ -3,6 +3,7 @@ package installation
 import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/satisfactorymodding/ficsit-cli/cli"
 )
@@ -17,6 +18,9 @@ var setVanillaCmd = &cobra.Command{
 	Use:   "set-vanilla <path>",
 	Short: "Set the installation to vanilla mode or not",
 	Args:  cobra.ExactArgs(1),
+	PreRun: func(cmd *cobra.Command, args []string) {
+		_ = viper.BindPFlag("off", cmd.Flags().Lookup("off"))
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		global, err := cli.InitCLI(false)
 		if err != nil {
@@ -35,12 +39,7 @@ var setVanillaCmd = &cobra.Command{
 			return errors.New("installation not found")
 		}
 
-		vanillaOff, err := cmd.Flags().GetBool("off")
-		if err != nil {
-			return err
-		}
-
-		installation.Vanilla = !vanillaOff
+		installation.Vanilla = !viper.GetBool("off")
 
 		return global.Save()
 	},
