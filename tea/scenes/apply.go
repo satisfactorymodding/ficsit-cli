e98c1c264a8108ad57d0df72eaaaa6435d795b31
Vilsol <me@vil.so>
2023-12-13 03:23:56 +0200

    chore: lint

--- tea/scenes/apply.go
+++ tea/scenes/apply.go
@@ -8,6 +8,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/muesli/reflow/wrap"
+
 	"github.com/satisfactorymodding/ficsit-cli/cli"
 	"github.com/satisfactorymodding/ficsit-cli/tea/components"
 	"github.com/satisfactorymodding/ficsit-cli/tea/scenes/keys"
